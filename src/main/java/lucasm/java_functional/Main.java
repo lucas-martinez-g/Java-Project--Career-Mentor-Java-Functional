65d4f8a6ac31c503906f90e2e90e6308c89985e0
lucas-martinez-g <lucas.martinez01@globant.com>
2024-08-15 14:02:04 -0300

    Added new MapUtils methods and better log visualization

--- src/main/java/lucasm/java_functional/Main.java
+++ src/main/java/lucasm/java_functional/Main.java
@@ -1,35 +1,54 @@
 package lucasm.java_functional;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
 public class Main {
 
+  static final Logger logger = LoggerUtils.getLogger();
+  static final String RESET = "\033[0m";
+  static final String BLUE = "\033[0;34m";
+
   public static void main(String[] args) {
-    final Logger logger = LoggerUtils.getLogger();
 
     WordsCounter wordsCounter = new WordsCounter("src/main/resources/txt");
 
     wordsCounter.updateWordsCounterMap();
 
     Map<String, Integer> map = wordsCounter.getWordsCounterMap();
-    logger.info("Map: %s".formatted(map.toString()));
+    logger.info(getFormattedString("Map: %s", map.toString()));
 
     Map<String, Integer> mapSortedByKey = MapUtils.sortMapByKeyComparator(map,
         Comparator.naturalOrder());
-    logger.info("Ordered by keys: %s".formatted(mapSortedByKey.toString()));
+    logger.info(getFormattedString("Ordered by keys: %s", mapSortedByKey.toString()));
 
     Map<String, Integer> mapSortedByKeyReversed = MapUtils.sortMapByKeyComparator(mapSortedByKey,
         Comparator.reverseOrder());
-    logger.info("Ordered reversed by keys: %s".formatted(mapSortedByKeyReversed.toString()));
+    logger.info(
+        getFormattedString("Ordered reversed by keys: %s", mapSortedByKeyReversed.toString()));
 
     Map<String, Integer> mapSortedByValues = MapUtils.sortMapByValueComparator(mapSortedByKey,
         Map.Entry.comparingByValue());
-    logger.info("Ordered by values: %s".formatted(mapSortedByValues.toString()));
+    logger.info(getFormattedString("Ordered by values: %s", mapSortedByValues.toString()));
 
     Map<String, Integer> mapSortedByValuesReversed = MapUtils.sortMapByValueComparatorReversed(
         mapSortedByKey, Map.Entry.comparingByValue());
-    logger.info("Ordered reversed by values: %s".formatted(mapSortedByValuesReversed.toString()));
+    logger.info(
+        getFormattedString("Ordered reversed by values: %s", mapSortedByValuesReversed.toString()));
+
+    List<String> listHigherValue = MapUtils.getKeysWithHigherValue(mapSortedByKey);
+    logger.info(getFormattedString("List keys with highest value: %s", listHigherValue.toString()));
+
+    List<String> listLowestValue = MapUtils.getKeysWithLowestValue(mapSortedByKey);
+    logger.info(getFormattedString("List keys with highest value: %s", listLowestValue.toString()));
+
+    Double average = MapUtils.getAverageValue(mapSortedByKey);
+    logger.info(getFormattedString("Average value: %s", average.toString()));
+  }
+
+  private static String getFormattedString(String s1, String s2) {
+    return BLUE + s1.formatted(s2) + RESET;
   }
 }
--- src/main/java/lucasm/java_functional/MapUtils.java
+++ src/main/java/lucasm/java_functional/MapUtils.java
@@ -1,8 +1,12 @@
 package lucasm.java_functional;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,6 +27,32 @@ public class MapUtils {
   public static <K, V> Map<K, V> sortMapByValueComparatorReversed(Map<K, V> map,
       Comparator<Map.Entry<K, V>> comparator) {
     return collectMap(map.entrySet().stream().sorted(comparator.reversed()));
+  }
+
+  public static <K, V extends Comparable<V>> List<K> getKeysWithHigherValue(Map<K, V> map) {
+    return getKeysWithRequiredValues(map, Collections.max(map.values()));
+  }
+
+  public static <K, V extends Comparable<V>> List<K> getKeysWithLowestValue(Map<K, V> map) {
+    return getKeysWithRequiredValues(map, Collections.min(map.values()));
+  }
+
+  public static <K, V extends Number> Double getAverageValue(Map<K, V> map) {
+    return map.values().stream()
+        .filter(Objects::nonNull)
+        .mapToDouble(Number::doubleValue)
+        .sum() / map.size();
+  }
+
+  private static <K, V extends Comparable<V>> List<K> getKeysWithRequiredValues(Map<K, V> map,
+      V valueToCompare) {
+    if (map.isEmpty()) {
+      return List.of();
+    }
+    return map.entrySet().stream()
+        .filter(entry -> entry.getValue().compareTo(valueToCompare) == 0)
+        .map(Entry::getKey)
+        .toList();
   }
 
   private static <K, V> Map<K, V> collectMap(Stream<Map.Entry<K, V>> stream) {
